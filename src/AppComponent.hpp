0289fc6bedae1d465196a19e8bac95b969fdfceb
lganzzzo <lganzzzo@gmail.com>
2020-10-24 02:08:00 +0300

    Update connection provider to use IPv4

--- src/AppComponent.hpp
+++ src/AppComponent.hpp
@@ -36,7 +36,7 @@ public:
    */
   OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, serverConnectionProvider)([] {
     /* non_blocking connections should be used with AsyncHttpConnectionHandler for AsyncIO */
-    return oatpp::network::tcp::server::ConnectionProvider::createShared({"localhost", 8000});
+    return oatpp::network::tcp::server::ConnectionProvider::createShared({"0.0.0.0", 8000, oatpp::network::Address::IP_4});
   }());
   
   /**
